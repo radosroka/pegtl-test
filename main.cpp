390708598e82cbdb8bd40a57e7ca53062b90c9d1
Radovan Sroka <rsroka@redhat.com>
2017-06-21 17:21:53 +0200

    Catching exception

--- main.cpp
+++ main.cpp
@@ -1,54 +1,53 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <locale>
 
 #include <tao/pegtl.hpp>
 
 using namespace tao::pegtl;
 
-namespace hello
+const std::string property = "usbguard";
+
+namespace usbguard
 {
-   // Parsing rule that matches a literal "Hello, ".
-
-   struct prefix
-         : tao::pegtl::string< 'H', 'e', 'l', 'l', 'o', ',', ' ' > {};
-
-   // Parsing rule that matches a non-empty sequence of
-   // alphabetic ascii-characters with greedy-matching.
-
-   struct name
-         : tao::pegtl::plus< tao::pegtl::alpha > {};
-
-   // Parsing rule that matches a sequence of the 'prefix'
-   // rule, the 'name' rule, a literal "!", and 'eof'
-   // (end-of-file/input), and that throws an exception
-   // on failure.
-
-   struct grammar
-         : tao::pegtl::must< prefix, name, tao::pegtl::one< '!' >, tao::pegtl::eof > {};
-
-   // Class template for user-defined actions that does
-   // nothing by default.
-
-   template< typename Rule >
-   struct action
-         : tao::pegtl::nothing< Rule > {};
-
-   // Specialisation of the user-defined action to do
-   // something when the 'name' rule succeeds; is called
-   // with the portion of the input that matched the rule.
-
-   template<> 
-   struct action< name >
+   namespace nsswitch
    {
-      template < typename Input >
-      static void apply(const Input & in, std::string & str )
-      {
-         str = in.string();
-      }
-   };
 
-} // hello
+      struct blanked
+            : tao::pegtl::star< tao::pegtl::blank > {};
+
+      struct prop_name
+            : tao::pegtl::istring< 'u', 's', 'b', 'g', 'u', 'a', 'r', 'd' > {};
+
+      struct value
+            : tao::pegtl::plus< tao::pegtl::alpha > {};
+
+
+      struct grammar
+            : tao::pegtl::must< blanked, prop_name, blanked, tao::pegtl::one< ':' >, blanked, value, blanked, tao::pegtl::eof > {};
+
+      template< typename Rule >
+      struct action
+            : tao::pegtl::nothing< Rule > {};
+
+      template<> 
+      struct action< value >
+      {
+         template < typename Input >
+         static void apply(const Input & in, std::string & str )
+         {
+            std::locale loc;
+            str = in.string();
+            for (int i = 0; i < str.length(); ++i)
+            {
+               str[i] = std::tolower(str[i], loc);
+            }
+         }
+      };
+   }
+
+}
 
 
 
@@ -62,12 +61,15 @@ int main(int argc, char const *argv[])
 	std::string line;
 
 	while (std::getline(file, line)) {
-		if (line.find("Hello") != std::string::npos && line[0] != '#') {
+		if (line[0] != '#') {
 			std::cout << line << std::endl;
          std::string name;
 			tao::pegtl::string_input<> in( line, "" );
-
-			tao::pegtl::parse< hello::grammar, hello::action >( in, name );
+         try {
+			   tao::pegtl::parse< usbguard::nsswitch::grammar, usbguard::nsswitch::action >( in, name );
+         } catch (tao::pegtl::parse_error &e) {
+            continue;
+         }
 			std::cout << "Good bye, " << name << "!" << std::endl;
 		}
 	}
